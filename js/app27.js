e95e21fa85a94ad6db16f5cb05b2d51f11592160
Maura <maurakummer@yahoo.com>
2022-05-05 11:05:47 +0200

    first commit, using the reverse() method

--- js/app27.js
+++ js/app27.js
@@ -1,11 +1,20 @@
-/*Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+/*
+****TASK***
 
-Examples:
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order.
+
+ Essentially, rearrange the digits to create the highest possible number.
+
+****EXAMPLES
+
 Input: 42145 Output: 54421
 
 Input: 145263 Output: 654321
 
-Input: 123456789 Output: 987654321*/
+Input: 123456789 Output: 987654321
+
+****PSEUDO CODE *****
+*/
 
 //Using the arrow function, first change the numbers to a string in order to convert to an array, as the higher order functions split()sort() only work on arrays. Then join the result and multiply by 1 which converts the string back to an arrray.
 
