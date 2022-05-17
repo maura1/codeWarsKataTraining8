17efeed50652c2ff414022217cb18d1f4047f36c
Maura <maurakummer@yahoo.com>
2022-05-17 09:24:07 +0200

    first commit, converting a string to uppercase

---
+++ js/app2.js
@@ -0,0 +1,12 @@
+/*Given a string convert it to uppercase
+Eg input = 'hello' , output = 'HELLO'
+
+Use the toUpperCase()method */
+
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+//Using the arrow function
+
+const makeUpperCase = (str) => str.toUpperCase();
