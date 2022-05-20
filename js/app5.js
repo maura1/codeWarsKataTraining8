3c8330a09fd1a89e1d313139dc499cc1c4265028
Maura <maurakummer@yahoo.com>
2022-05-20 10:45:50 +0200

    first commit, using tenary operator

--- js/app5.js
+++ js/app5.js
@@ -21,3 +21,27 @@ const rps = (p1, p2) =>
         : p1 === 'paper' && p2 === 'rock'
         ? 'Player 1 won!'
         : 'Player 2 won!';
+/*Title: How much water di I need
+My washing machine uses water amount of water to wash load (in JavaScript and Python) or max_load (in Ruby) amount of clothes. You are given a clothes amount of clothes to wash. For each single item of clothes above the load, the washing machine will use 10% more water (multiplicative) to clean.
+
+For example, if the load is 10, the amount of water it requires is 5 and the amount of clothes to wash is 14, then you need 5 * 1.1 ^ (14 - 10) amount of water.
+
+Write a function howMuchWater (JS)/how_much_water (Python and Ruby) to work out how much water is needed if you have a clothes amount of clothes. The function will accept 3 arguments: - water, load (or max_loadin Ruby) and clothes.
+
+My washing machine is an old model that can only handle double the amount of load (or max_load). If the amount of clothes is more than 2 times the standard amount of load (max_load), return 'Too much clothes'. The washing machine also cannot handle any amount of clothes less than load (max_load). If that is the case, return 'Not enough clothes'.
+
+The answer should be rounded to the nearest 2 decimal places.
+ 
+The input will be 3 integers
+
+Expected output:
+    (10,10,21) => 'Too much clothes'
+    (10,10,5) => 'Not enough clothes'
+    (10,5,8)  => 13.31
+
+Use the arrow function with tenary operator*/
+
+const howMuchWater = (water, load, clothes) =>
+       clothes > 2 * load ? 'Too much clothes'
+    : (clothes < load ? 'Not enough clothes' 
+    : Number((water * 1.1 ** (clothes - load)).toFixed(2)))
